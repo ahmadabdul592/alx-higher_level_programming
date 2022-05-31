800faba8ac31cb3ddc77a2b47821d25d61fb8dd3
ahmadabdul592 <ahmadabdul592@gmail.com>
2022-05-31 15:20:11 -0700

    Conditional, Loops and Functions Implementation

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 9):
     for j in range(i + 1, 10):
-        if i == 8:
-            print(f"{i:d}{j:d}")
+g       if i == 8:
+            print("{:d}{:d}".format(i, j))
         else:
-            print(f"{i:d}{j:d}", end=', ')
+            print("{:d}{:d}".format(i, j), end=', ')
