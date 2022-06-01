5b9634bd61dcb8202f8e6ce94f6157a8c0da284f
Abdul Ahmed Abdul <34511653+ahmadabdul592@users.noreply.github.com>
2022-06-01 21:32:32 +0100

    Conditional, Loops and Functions Implementation

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    for c in str:
-        if ord(c) >= ord('a') and ord(c) <= ord('z'):
-            char = chr(ord(c) - 32)
-        else:
-            char = c
-        print("{:s}".format(char), end="")
-    print(' ')
+    for i in str:
+        print('{:s}'.format(chr(ord(i) - 32) if ord(i) >= ord('a') and
+                            ord(i) <= ord('z') else i), end="")
+    print("".format(), end="\n")
